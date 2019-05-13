6be3205644f56c247f39de28d81e7193888e0b13
mithil2311 <DX1301c2>
2019-05-13 14:42:14 +0530

    add

--- fprd/template/footer.php
+++ fprd/template/footer.php
@@ -2,16 +2,24 @@
   <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
+<<<<<<< HEAD
      Made by PI CLUB
     </div>
     <!-- Default to the left -->
     <!-- <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. -->
+=======
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+>>>>>>> bac95bbfe8dce14c28cf90ed8bda049ef44f02f8
   </footer>
 </div>
 <!-- ./wrapper -->
 
 <!-- REQUIRED SCRIPTS -->
 
+<<<<<<< HEAD
 <script src="<?php echo fpath;?>template/plugins/fastclick/fastclick.js"></script>
 
 <!-- jQuery -->
@@ -20,12 +28,26 @@
 <script src="<?php echo fpath;?>template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo fpath;?>template/dist/js/adminlte.min.js"></script>
+=======
+<script src="plugins/fastclick/fastclick.js"></script>
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+>>>>>>> bac95bbfe8dce14c28cf90ed8bda049ef44f02f8
 <!-- jQuery -->
 
 
 
 <!-- AdminLTE for demo purposes -->
+<<<<<<< HEAD
 <script src="<?php echo fpath;?>template/dist/js/demo.js"></script>
+=======
+<script src="dist/js/demo.js"></script>
+>>>>>>> bac95bbfe8dce14c28cf90ed8bda049ef44f02f8
 <!-- page script -->
 
 </body>
--- fprd/template/header.php
+++ fprd/template/header.php
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 <?php 
 define('fpath','http://localhost/fprd/');
 ?>
+=======
+
+>>>>>>> bac95bbfe8dce14c28cf90ed8bda049ef44f02f8
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
@@ -12,6 +16,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
+<<<<<<< HEAD
   <title><?php echo $title;?></title>
 
   <!-- Font Awesome Icons -->
@@ -23,4 +28,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+=======
+  <title><?php $title;?></title>
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+>>>>>>> bac95bbfe8dce14c28cf90ed8bda049ef44f02f8
 </head>
